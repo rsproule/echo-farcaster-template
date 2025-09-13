9750cae80d08973d3b70b3e6ef9f2f29235cd69e
Ryan Sproule <rsproule23@gmail.com>
2025-09-13 13:38:14 -0400

    external sign in

--- src/components/ui/EchoSignInButton.tsx
+++ src/components/ui/EchoSignInButton.tsx
@@ -2,17 +2,15 @@
 
 import { sdk } from "@farcaster/miniapp-sdk";
 import { Button } from "./Button";
-import { signIn } from "@merit-systems/echo-next-sdk/client";
 
 export function EchoSignInButton() {
   return (
     <Button
       onClick={async () => {
         console.log("signing in with echo");
-        // await sdk.actions.openUrl({
-        //   url: "https://echo-farcaster-template.vercel.app/api/echo/signin",
-        // });
-        signIn();
+        await sdk.actions.openUrl({
+          url: "https://echo-farcaster-template.vercel.app/api/echo/signin",
+        });
       }}
       size="lg"
     >
