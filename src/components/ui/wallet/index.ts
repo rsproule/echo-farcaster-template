451c3e4906e4c10d6976705aa29716c152fbd260
Ryan Sproule <rsproule23@gmail.com>
2025-09-13 12:44:18 -0400

    dletel

--- src/components/ui/wallet/index.ts
+++ src/components/ui/wallet/index.ts
@@ -1,5 +1,4 @@
-export { SignIn } from './SignIn';
-export { SignEvmMessage } from './SignEvmMessage';
-export { SendEth } from './SendEth';
-export { SignSolanaMessage } from './SignSolanaMessage';
-export { SendSolana } from './SendSolana'; 
+export { SendEth } from "./SendEth";
+export { SignEvmMessage } from "./SignEvmMessage";
+export { SignIn } from "./SignIn";
+export { SignSolanaMessage } from "./SignSolanaMessage";
