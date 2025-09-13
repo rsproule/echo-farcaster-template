42daba5cb38045963fcb799e7323ee43c6e106a9
Ryan Sproule <rsproule23@gmail.com>
2025-09-13 12:53:20 -0400

    esm

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,4 +1,4 @@
-import { type AccountAssociation } from "@farcaster/miniapp-core/esm/manifest";
+import { type AccountAssociation } from "@farcaster/miniapp-core/dist/manifest";
 
 /**
  * Application constants and configuration values.
